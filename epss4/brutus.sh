48c2f3284e686c54a03c826ec4f46439dac98b46
Daniel Harenberg, ETH Zurich <dharenberg@ethz.ch>
2012-10-18 11:56:10 +0200

    Adjusted Brutus and Cheops scripts
    Can choose the Build configuration now

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #
 
+thisrun='test4b'
+BUILD=Build_Optimmax
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
-
 projectname="${MYDIR##*/}"
-
-thisrun='01b-gelsy-i13c' # "$projectname"
 
 mkdir --parents ~/Tempdh/Brutus/$thisrun/model_input_old
 cd $MYDIR
 cp -r model_input/* ~/Tempdh/Brutus/$thisrun/model_input_old
-rm Build_Optim/*.o Build_Optim/*.mod Build_Optim/src_utilities/*.o Build_Optim/src_utilities/*.mod
+rm $BUILD/*.o $BUILD/*.mod $BUILD/src_utilities/*.o $BUILD/src_utilities/*.mod
 mv br-* br-$thisrun
 sed -i "s/epss\w*/$projectname/g" br-$thisrun
 
-tar -cf - *.f90 br-$thisrun Build_Optim Matlab src_utilities model_input/*.* model_input/calib* model_input/data model_input/last_results/*.* | ssh -C danielh@brutus.ethz.ch "mkdir -p $thisrun/model_output $thisrun/model_input/last_results/previous && cd $thisrun && tar -xf - ; bsub -J $thisrun < br-$thisrun"
+tar -cf - *.f90 br-$thisrun $BUILD Matlab src_utilities model_input/*.* model_input/calib* model_input/data model_input/last_results/*.* | ssh -C danielh@brutus.ethz.ch "export BUILD=$BUILD && mkdir -p $thisrun/model_output $thisrun/model_input/last_results/previous && cd $thisrun && tar -xf - ; bsub -J $thisrun < br-$thisrun"
--- epss4/cheops.sh
+++ epss4/cheops.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #
 
+thisrun='tau002test2c'
+BUILD=Build_Optimmax
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
-
 projectname="${MYDIR##*/}"
-
-thisrun='tau002test2c' # "$projectname"
 
 mkdir --parents ~/Tempdh/Cheops/$thisrun/model_input_old
 cd $MYDIR
 cp -r model_input/* ~/Tempdh/Cheops/$thisrun/model_input_old
-rm Build_Optim/*.o Build_Optim/*.mod Build_Optim/src_utilities/*.o Build_Optim/src_utilities/*.mod
+rm $BUILD/*.o $BUILD/*.mod $BUILD/src_utilities/*.o $BUILD/src_utilities/*.mod
 mv ch-* ch-$thisrun
 sed -i "s/epss\w*/$projectname/g" ch-$thisrun
 
-tar -cf - *.f90 ch-$thisrun Build_Optim Matlab src_utilities model_input/*.* model_input/calib* model_input/data model_input/last_results/*.* | ssh -C dharenbe@cheops.rrz.uni-koeln.de "mkdir -p $thisrun/model_output $thisrun/model_input/last_results/previous && cd $thisrun && tar -xf - ; qsub ch-$thisrun" 
+tar -cf - *.f90 ch-$thisrun $BUILD Matlab src_utilities model_input/*.* model_input/calib* model_input/data model_input/last_results/*.* | ssh -C dharenbe@cheops.rrz.uni-koeln.de "export BUILD=$BUILD && mkdir -p $thisrun/model_output $thisrun/model_input/last_results/previous && cd $thisrun && tar -xf - ; qsub ch-$thisrun" 
