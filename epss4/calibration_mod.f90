b60a56eb2ed6279d2916f3b37dc650d1efb5e4ee
Daniel Harenberg <dharenberg@ethz.ch>
2013-01-15 16:21:23 +0100

    Removed print statements for debugging

--- epss4/calibration_mod.f90
+++ epss4/calibration_mod.f90
@@ -41,12 +41,6 @@ contains
         print '(t2,a)','- calibration: starting root finder'
 
         call read_data_targets(n_end_params, data_targets)
-        !DEBUG
-        print*, 'params'
-        print*, xvals
-        print*, 'data'
-        print*, data_targets
-        ! END DEBUG
 
         ! Initialize root finder
         it=0
@@ -88,22 +82,10 @@ contains
             call set_params(param_vec)
             print *,''
             print '(a,i3.3)','Calibration iteration ', it
-            !DEBUG
-            print*, 'params'
-            print*, param_vec
-            ! END DEBUG
 
             call run_model(projectname, calib_name, welfare_temp, simvars)
 
             distance = distance_norm(data_targets, model_targets(n_end_params, simvars))
-            !DEBUG
-            print*, 'data'
-            print*, data_targets
-            print*, 'model'
-            print*, model_targets(n_end_params, simvars)
-            print*, 'distance'
-            print*, distance
-            ! END DEBUG
 
             ! if (save_all_iterations) call save_intermediate_results(it, distance, coeffs, coeffs_old, Phi, simvars, grids, lifecycles, policies, err, calib_name, projectname)
 
