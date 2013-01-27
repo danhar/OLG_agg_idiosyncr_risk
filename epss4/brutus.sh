2be725ef873bf3b9cab4c26c4c3cdf33bfdf8585
Daniel Harenberg <dharenberg@ethz.ch>
2013-01-27 18:12:38 +0100

    Different runs on brutus

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 
-thisrun='030_nk8_nthreads8'
+thisrun='030_comp'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
-NTHREADS=8  # number of OpenMP threads 
+NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
 projectname="${MYDIR##*/}"
 
