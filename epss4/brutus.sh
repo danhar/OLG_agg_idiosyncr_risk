09419ad8794cc9dafd9906c0e78041ca72566732
Daniel Harenberg <dharenberg@ethz.ch>
2013-03-13 17:22:45 +0100

    different runs on brutus

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='040-ies05d_nosurv'
+thisrun='041-ies05n3'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
