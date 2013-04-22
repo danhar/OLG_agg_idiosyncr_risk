87ce64b555a774e02af9452ce9cea5b740332ab1
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-04-22 16:59:08 +0200

    different runs

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='049-I_Y4_maxstp2'
+thisrun='050-I_Y-03b'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
