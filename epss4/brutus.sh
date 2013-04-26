ea10eefa33e476bbfdad36ef7f53d0b3c0a9734a
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-04-26 19:35:04 +0200

    base_ep calibration

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='054-ies15_sharpe8h'
+thisrun='055-ies05_ep'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
