b65f9f5b5f5ac358ecbf9f7dd234d9fed3505cd4
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-04-26 20:25:54 +0200

    new runs

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='055-ies05_ep'
+thisrun='055-surv_crra05'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
