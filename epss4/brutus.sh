3b53ab757d38da96a1e62ec71729634170889cac
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-06-06 14:35:53 +0200

    new calib

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='066-test_KS_fixpoint_gs2'
+thisrun='066-noIR_good'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
