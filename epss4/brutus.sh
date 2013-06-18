ade9ee98a1eb1f68e1695012e3f5d04649053095
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-06-18 08:48:39 +0200

    first run

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='066-noIR_sharpe2'
+thisrun='067-base_bc'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
