6777ad1772bc3a42e9e4decb6a84ff6bd746a044
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-07-30 18:31:23 +0200

    worked on insurance_effect, but tired. builds but maybe wrong

--- epss4/insurance_effect_mod.f90
+++ epss4/insurance_effect_mod.f90
@@ -8,10 +8,11 @@ contains
 subroutine calc_insurance_effect(policies, value, agg_grid, simvars_in, Phi_in, calib_name, projectname, welfare)
     use ifport            ,only: system  ! Intel Fortran portability library
     use kinds            ,only: dp
-    use classes_mod      ,only: tSimvars, tLifecycle, tPolicies, tAggGrids
+    use classes_mod      ,only: tSimvars, tLifecycle, tPolicies, tAggGrids, tStats
     use lifecycles_class ,only: average
     use simvars_class    ,only: print_error
-    use params_mod       ,only: exogenous_xgrid, nx_factor, stat_dist_z, stat_dist_eta, ccv, scale_IR, scale_AR, scale_IR_orig,&
+    use fun_locate
+    use params_mod       ,only: exogenous_xgrid, nx_factor, stat_dist_z, stat_dist_eta, ccv, scale_IR, scale_AR, scale_IR_orig, etagrid, &
                                 params_set, params_set_thisrun, CheckParams, construct_path
     use simulation_mod   ,only: simulate
     use interpolate_xgrid
@@ -25,13 +26,14 @@ subroutine calc_insurance_effect(policies, value, agg_grid, simvars_in, Phi_in, 
     real(dp)         ,intent(out) :: welfare(4)
     type(tLifecycle)              :: lifecycles, lifecycles_array(size(simvars_in))         ! lifecycle profiles
     type(tPolicies)               :: pol_fine, pol_minus_risk    ! if (exogenous_xgrid) then this will hold interpolated policies
+    type(tStats)                  :: K, mu, netwage, pens, bequests, r, rf
     type(tSimvars) ,allocatable   :: simvars(:)
     real(dp)       ,allocatable   :: Phi(:,:,:)
-    real(dp), allocatable     :: val_newx(:,:,:,:,:,:), xgrid_mean_old(:,:,:), xgrid_mean_new(:,:,:), Phi_spread(:,:,:,:)
+    real(dp), allocatable     :: val_newx(:,:,:,:,:,:), val_minus_risk(:,:,:,:,:,:), xgrid_mean_old(:,:,:), xgrid_mean_new(:,:,:), Phi_spread(:,:,:,:)
     character(len=7)          :: runchar
     character(:),allocatable  :: output_path
     logical                   :: ccv_thisrun
-    real(dp)                  :: scale_IR_thisrun, scale_AR_thisrun
+    real(dp)                  :: scale_IR_thisrun, scale_AR_thisrun, w
     integer                   :: syserr, i
 
     Phi     = Phi_in    ! initial values, makes clear that Phi is not output
@@ -41,6 +43,14 @@ subroutine calc_insurance_effect(policies, value, agg_grid, simvars_in, Phi_in, 
     ccv_thisrun = ccv
     scale_IR_thisrun = scale_IR
     scale_AR_thisrun = scale_AR
+
+    K%name ='K'; call K%calc_stats(simvars)
+    mu%name='mu'; call mu%calc_stats(simvars)
+    netwage%name='wage'; call netwage%calc_stats(simvars)
+    pens%name='pens'; call pens%calc_stats(simvars)
+    bequests%name='bequests'; call bequests%calc_stats(simvars)
+    r%name='r'; call r%calc_stats(simvars)
+    rf%name='rf'; call rf%calc_stats(simvars)
 
     print *,'- insurance_effect: simulating'
 
@@ -58,6 +68,23 @@ subroutine calc_insurance_effect(policies, value, agg_grid, simvars_in, Phi_in, 
 
     call params_set('scale_AR', -1.0_dp)
     pol_minus_risk = policies%mean(3,stat_dist_z)
+    pol_minus_risk = pol_minus_risk%interpolate(5,agg_grid%k , K%avg_exerr_())
+    pol_minus_risk = pol_minus_risk%interpolate(6,agg_grid%mu,mu%avg_exerr_())
+    val_minus_risk = value
+    val_minus_risk = 0.0
+    do i=1,size(stat_dist_z)
+        val_minus_risk(:,:,1,:,:,:)= val_minus_risk(:,:,1,:,:,:) + stat_dist_z(i)*value(:,:,i,:,:,:)
+    enddo
+    val_minus_risk(:,:,2:,:,:,:) = spread(val_minus_risk(:,:,1,:,:,:),3, size(stat_dist_z)-1)
+
+    i=f_locate(agg_grid%k,K%avg_exerr_())
+    w=(K%avg_exerr_()-agg_grid%k(i))/(agg_grid%k(i+1)-agg_grid%k(i))
+    val_minus_risk(:,:,:,:,:,:) = spread((1-w)*val_minus_risk(:,:,:,:,i,:) + w*val_minus_risk(:,:,:,:,i+1,:),5,size(agg_grid%k))
+
+    i=f_locate(agg_grid%mu,mu%avg_exerr_())
+    w=(mu%avg_exerr_()-agg_grid%mu(i))/(agg_grid%mu(i+1)-agg_grid%mu(i))
+    val_minus_risk(:,:,:,:,:,:) = spread((1-w)*val_minus_risk(:,:,:,:,:,i) + w*val_minus_risk(:,:,:,:,:,i+1),6,size(agg_grid%mu))
+
     if (scale_IR_orig .ne. -1.0) call params_set('scale_IR', 0.0_dp)
     write(runchar,'(a5)') ',noAR'
     call sim_pe(welfare(3))
@@ -77,38 +104,49 @@ subroutine calc_insurance_effect(policies, value, agg_grid, simvars_in, Phi_in, 
     contains
 
     subroutine sim_pe(welf)
+        use distribution           ,only: TransitionPhi
         real(dp) ,intent(out) ::welf
         output_path=construct_path(calib_name,'insurance')//runchar
         syserr = system('mkdir '//output_path//' > /dev/null 2>&1') ! Creates directory for output files, suppresses error if dir exists
         call params_set_thisrun
         call CheckParams
+        if (runchar == 'noCCV' .or. runchar=='noIR') then
+            if (exogenous_xgrid) then
+                ! This is the standard case which should always be used, because we make the xgrid much finer
+                call InterpolateXgrid(nx_factor, pol_minus_risk, value, pol_fine, val_newx)
+                ! We also want the initial Phi that we take from previous simulations to be defined over the new xgrid
+                xgrid_mean_new = sum(pol_fine%xgrid(:,:,1,:,:,1),4)/size(pol_fine%xgrid,5) ! only an approximation of the grid over which Phi is defined
+                call InterpolateXgrid(Phi, xgrid_mean_old, xgrid_mean_new)
+                Phi_spread = spread(Phi,4,size(simvars))
+                !$OMP  PARALLEL DO IF(size(simvars)>1)
+                do i=1,size(simvars)
+                    call simulate(pol_fine, val_newx, agg_grid, simvars(i), Phi_spread(:,:,:,i), lifecycles_array(i))
+                enddo
+                !$OMP END PARALLEL DO
+            else
+                ! In this case, we simultaneously solve for the rf(t+1) (actually the mu(t+1)) and the corresponding xgrid(since it depends on mu).
+                ! That is very costly, so we do not refine xgrid. While more correct theoretically, the coarse xgrid makes the solution less precise.
+                Phi_spread = spread(Phi,4,size(simvars))
+                !$OMP  PARALLEL DO IF(size(simvars)>1)
+                do i=1,size(simvars)
+                    call simulate(policies, value, agg_grid, simvars(i), Phi_spread(:,:,:,i), lifecycles_array(i))
+                enddo
+                !$OMP END PARALLEL DO
+            endif
+            lifecycles=average(lifecycles_array)
+            Phi = sum(Phi_spread,4)/size(Phi_spread,4)
 
-        if (exogenous_xgrid) then
-            ! This is the standard case which should always be used, because we make the xgrid much finer
-            call InterpolateXgrid(nx_factor, pol_minus_risk, value, pol_fine, val_newx)
-            ! We also want the initial Phi that we take from previous simulations to be defined over the new xgrid
-            xgrid_mean_new = sum(pol_fine%xgrid(:,:,1,:,:,1),4)/size(pol_fine%xgrid,5) ! only an approximation of the grid over which Phi is defined
-            call InterpolateXgrid(Phi, xgrid_mean_old, xgrid_mean_new)
-            Phi_spread = spread(Phi,4,size(simvars))
-            !$OMP  PARALLEL DO IF(size(simvars)>1)
-            do i=1,size(simvars)
-                call simulate(pol_fine, val_newx, agg_grid, simvars(i), Phi_spread(:,:,:,i), lifecycles_array(i))
-            enddo
-            !$OMP END PARALLEL DO
+            welf = calc_average_welfare(simvars)
         else
-            ! In this case, we simultaneously solve for the rf(t+1) (actually the mu(t+1)) and the corresponding xgrid(since it depends on mu).
-            ! That is very costly, so we do not refine xgrid. While more correct theoretically, the coarse xgrid makes the solution less precise.
-            Phi_spread = spread(Phi,4,size(simvars))
-            !$OMP  PARALLEL DO IF(size(simvars)>1)
-            do i=1,size(simvars)
-                call simulate(policies, value, agg_grid, simvars(i), Phi_spread(:,:,:,i), lifecycles_array(i))
-            enddo
-            !$OMP END PARALLEL DO
-        endif
-        lifecycles=average(lifecycles_array)
-        Phi = sum(Phi_spread,4)/size(Phi_spread,4)
 
-        welf = calc_average_welfare(simvars)
+            call InterpolateXgrid(nx_factor, pol_minus_risk, val_minus_risk, pol_fine, val_newx)
+
+            ! Alternatively, one could take Phi from the noCCV simulations, which is an average, but I think the following is more correct
+            Phi = TransitionPhi(rf%avg_exerr_(),r%avg_exerr_(),netwage%avg_exerr_(),pens%avg_exerr_(),bequests%avg_exerr_(),pol_minus_risk%xgrid(:,:,1,:,1,1),pol_minus_risk%apgrid(:,:,1,:,1,1),pol_minus_risk%stocks(:,:,1,:,1,1),etagrid(:,1))
+
+            welf = sum(val_newx(:,:,1,1,1,1)*Phi(:,:,1))
+
+        endif
     end subroutine sim_pe
 
     !-------------------------------------------------------------------------------
