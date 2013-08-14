281e4632146dc3976d1c6378728bd5045b69e663
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-08-14 18:16:37 +0200

    testrun and cleanup

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='069-test'
+thisrun='073-baseline_calib_precise'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
