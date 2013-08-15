90d121e0a61a4447e3276698871ef3dbf54736fa
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-08-15 18:50:32 +0200

    Fucking brutus.sh now only copies the corresponding last_results
    Because all model_input/last_results is too big. This was a lot of work.

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='073-baseline_calib_precise'
+thisrun='073-baseline_calib_precise10'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
@@ -9,10 +9,17 @@ projectname="${MYDIR##*/}"
 
 mkdir --parents ~/Tempdh/Brutus/$thisrun
 cd $MYDIR
-tar -zcf code.tar.gz *.f90 br* ch* src* model_input/last_results/*.*
+
+cd model_input/last_results
+# The next line is crazy: it finds the name of all selected calibrations (not outcommented, and without the .txt) and pipes them to tar. Note it handles also several calibs.
+grep -Po '^[^!].*?(?<=/)\K[^/.]*(?=\.)' ../select_calibration_here.txt | xargs tar -cjf last_results.tar.bz2
+cd ../..
+
+tar -zcf code.tar.gz *.f90 br* src*
 mv code.tar.gz ~/Tempdh/Brutus/$thisrun
 rm $BUILD/*.o $BUILD/*.mod $BUILD/src_utilities/*.o $BUILD/src_utilities/*.mod
 mv br-* br-$thisrun
 sed -i "s/epss\w*/$projectname/g" br-$thisrun
 
-tar -cf - *.f90 br-$thisrun $BUILD src_classes src_matlab src_utilities model_input/*.* model_input/calib* model_input/data model_input/last_results/*.* | ssh -C danielh@brutus.ethz.ch "export BUILD=$BUILD && export OMP_NUM_THREADS=$NTHREADS && mkdir -p $thisrun/model_output $thisrun/model_input/last_results/previous && cd $thisrun && tar -xf - ; bsub -J $thisrun < br-$thisrun"
+tar -cf - *.f90 br-$thisrun $BUILD src_classes src_matlab src_utilities model_input/*.* model_input/calib* model_input/data model_input/last_results/last_results.tar.bz2 | ssh -C danielh@brutus.ethz.ch "export BUILD=$BUILD && export OMP_NUM_THREADS=$NTHREADS && mkdir -p $thisrun/model_output && cd $thisrun && tar -xf - ; cd model_input/last_results; tar -xjf last_results.tar.bz2; rm last_results.tar.bz2; cd ../..; bsub -J $thisrun < br-$thisrun"
+rm model_input/last_results/last_results.tar.bz2
