1c902beef5526fcf0427cd8ec0ce8f2e52ea07ac
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-08-19 18:21:22 +0200

    All calibratoins with n_eta=5 and higher accuracy

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='073-base_neta4'
+thisrun='074-noIR_neta4_7d3'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
