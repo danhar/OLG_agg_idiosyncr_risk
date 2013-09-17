5f9ea594d0007e5223506105049377826301efea
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-09-17 18:00:59 +0200

    different calibrations

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='074-ies15_ep_recal3'
+thisrun='074-ies15_sharpe_lower_prec3'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
