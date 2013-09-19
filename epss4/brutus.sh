eb25c057eb729bb71aa899754d97b3047d8887f7
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-09-19 20:13:05 +0200

    different runs

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='074-ies15_sharpe_lower_prec3'
+thisrun='078-baseline_euler_err3'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
