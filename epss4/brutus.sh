bd837fd57299e490eaf693c39123adf872e64bea
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-09-27 17:00:08 +0200

    runs with old version

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='080-base_ep_nx4_7d'
+thisrun='081-noAR_eul_old'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
