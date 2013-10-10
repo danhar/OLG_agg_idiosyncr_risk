af8d91904b6606ffaa0ad86644c9e0d2717609b8
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-10-10 12:03:29 +0200

    different runs

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='082-base_eerr_corr'
+thisrun='082-ies15sh_eerr_corr'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
