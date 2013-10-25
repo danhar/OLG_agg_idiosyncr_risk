9e6b73e1715cbfdd38f3e0a5a4b4b1b83de7c9a6
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-10-25 17:25:50 +0200

    different runs. Theta=1 new

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='082-tau12_recal2_n3'
+thisrun='082-theta1_lp'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
