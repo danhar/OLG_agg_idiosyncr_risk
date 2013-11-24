77b6aa4ff4b56a7dc01cbf4a47438aff374db5cd
Daniel Harenberg (office) <dharenberg@ethz.ch>
2013-11-24 18:56:23 +0100

    higher prec cal files, the first 6 in our overview

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='085-base_sharpe_fine'
+thisrun='086-ies_ep_7d'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug #    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
