5c95b8a347697a1ce300c495266c8009d606e377
Daniel Harenberg (Laptop) <dharenberg@ethz.ch>
2015-08-03 21:29:08 +0200

    collapse didnt speed up at all

--- epss4/euler.sh
+++ epss4/euler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='090-GM1'
+thisrun='090-base_ep_collapse'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug # Parallel_Optim    
 NTHREADS=24  # number of OpenMP threads 
 # OMP_STACKSIZE=16M #stack size for each OMP thread: default 4M, recommended 16M, mytest 512M
