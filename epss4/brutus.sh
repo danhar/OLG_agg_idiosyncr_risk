4c11112d9743941ebaeb3e006bf3b799f881f9ff
Daniel Harenberg (office) <dharenberg@ethz.ch>
2016-04-07 14:16:59 +0200

    run calib tau12_recal to get var(log(cons)) profiles. worked.

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='090-base_Alex_income'
+thisrun='091-tau12_recal'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug # Parallel_Optim    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
