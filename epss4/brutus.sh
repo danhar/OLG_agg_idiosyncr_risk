95ce2f5d91f1f284961d364c5dc5cc11613af687
Daniel Harenberg (office) <dharenberg@ethz.ch>
2016-04-12 12:45:24 +0200

    jobs

--- epss4/brutus.sh
+++ epss4/brutus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-thisrun='091-tau12_recal'
+thisrun='091-baseline_corr2'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug # Parallel_Optim    
 NTHREADS=16  # number of OpenMP threads 
 MYDIR="$( cd "$( dirname "$0" )" && pwd )"
