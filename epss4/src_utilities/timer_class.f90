d04f858244dd83e2c7e4f2b68b3fb17888f83fe1
Daniel Harenberg (Laptop) <dharenberg@ethz.ch>
2017-05-26 09:05:40 +0200

    added wall time to timer class

--- epss4/src_utilities/timer_class.f90
+++ epss4/src_utilities/timer_class.f90
@@ -2,6 +2,7 @@
 !////////////////////////////////////////////////////////////////////////
 !      timer_class.f90
 !      Created: September 2, 2013 1:58 PM By: NocturnalAviationSoftware  
+!      Modified: Mai 24, 2017 by Daniel Harenberg
 !
 !      See description and example on:
 !      http://www.nocturnalaviationsoftware.com/free-fortran-code.html
@@ -9,6 +10,7 @@
 !      Defines a class for timing Fortran program execution
 !      Attention: uses cpu_time (which differs depending on how many threads there are) , not system_clock (which measures wall clock time)
 !      Therefore good for timing algorithms /code sections, in particular PURE ones.
+!      Modification by DH allows system_clock.
 !
 !      Usage
 !         * Initialization *
@@ -56,13 +58,30 @@
       INTEGER, PARAMETER, PUBLIC  :: TC_SECONDS = 0, TC_MINUTES = 1, TC_HOURS = 2
 !
 !     ---------------------
-!     Class type definition
+!     Class type definitions
 !     ---------------------
 !
-      TYPE, PUBLIC :: t_timer
+      TYPE, PUBLIC :: t_timer_cpu
          LOGICAL      , PRIVATE :: started = .FALSE., stopped = .FALSE.
          REAL(KIND=TP), PRIVATE :: startTime = 0.0_TP
          REAL(KIND=TP), PRIVATE :: finishTime = 0.0_TP
+!
+!        ========
+         CONTAINS
+!        ========
+!
+         PROCEDURE, PASS :: start => startTimer_cpu
+         PROCEDURE, PASS :: stop  => stopTimer_cpu
+         PROCEDURE, PASS :: elapsedTime => elapsedTime_cpu
+
+      END TYPE t_timer_cpu
+!
+
+      TYPE, PUBLIC :: t_timer
+         LOGICAL      , PRIVATE :: started = .FALSE., stopped = .FALSE.
+         INTEGER      , PRIVATE :: startTime
+         INTEGER      , PRIVATE :: finishTime
+         INTEGER      , PRIVATE :: countRate
 !
 !        ========
          CONTAINS
@@ -81,25 +100,89 @@
 !
 !////////////////////////////////////////////////////////////////////////  
 !  
+      SUBROUTINE startTimer_cpu(self)
+         IMPLICIT NONE
+         CLASS(t_timer_cpu) :: self
+         self % started = .TRUE.
+         CALL CPU_TIME(self % startTime)
+      END SUBROUTINE startTimer_cpu
+
       SUBROUTINE startTimer(self)  
          IMPLICIT NONE
          CLASS(t_timer) :: self
          self % started = .TRUE.
-         CALL CPU_TIME(self % startTime)         
+         CALL SYSTEM_CLOCK(self % startTime, self % countRate)
       END SUBROUTINE startTimer
+
 !
 !////////////////////////////////////////////////////////////////////////  
 !  
+      SUBROUTINE stopTimer_cpu(self)
+         IMPLICIT NONE
+         CLASS(t_timer_cpu) :: self
+         CALL CPU_TIME(self % finishTime)
+         self % stopped = .TRUE.
+      END SUBROUTINE stopTimer_cpu
+!
       SUBROUTINE stopTimer(self)  
          IMPLICIT NONE
          CLASS(t_timer) :: self
-         CALL CPU_TIME(self % finishTime)
+         CALL SYSTEM_CLOCK(self % finishTime)
          self % stopped = .TRUE.
       END SUBROUTINE stopTimer
-!
+
 !//////////////////////////////////////////////////////////////////////// 
 ! 
-       FUNCTION elapsedTime(self,units)  
+       FUNCTION elapsedTime_cpu(self,units) RESULT(elapsedTime)
+         IMPLICIT NONE
+!
+!        ---------
+!        Arguments
+!        ---------
+!
+         CLASS(t_timer_cpu)    :: self
+         INTEGER, OPTIONAL :: units
+         REAL(KIND=TP)  :: elapsedTime
+!
+!        ------------------------------------------
+!        Return zero if the timer was never started
+!        ------------------------------------------
+!
+         IF ( .NOT.self % started )     THEN
+            elapsedTime = 0.0_TP
+            RETURN
+         END IF
+!
+!        ----------------------------------------------
+!        If the timer was not stopped, then return the
+!        current time elapsed
+!        ----------------------------------------------
+!
+         IF ( .NOT.self % stopped )     THEN
+            CALL self % stop()
+         END IF
+
+         elapsedTime =  self % finishTime - self % startTime
+!
+!        -------------------------------------
+!        Convert to requested units if present
+!        -------------------------------------
+!
+         IF ( PRESENT(units) )     THEN
+
+            SELECT CASE ( units )
+               CASE( TC_MINUTES )
+                  elapsedTime = elapsedTime/60.0_TP
+               CASE( TC_HOURS )
+                  elapsedTime = elapsedTime/3600.0_TP
+               CASE DEFAULT
+
+            END SELECT
+         END IF
+
+      END FUNCTION elapsedTime_cpu
+
+      FUNCTION elapsedTime(self,units)
          IMPLICIT NONE
 !
 !        ---------
@@ -128,7 +211,7 @@
             CALL self % stop() 
          END IF 
 
-         elapsedTime =  self % finishTime - self % startTime
+         elapsedTime =  real(self % finishTime - self % startTime, TP) / real(self % countRate, TP)
 !
 !        -------------------------------------
 !        Convert to requested units if present
@@ -147,5 +230,6 @@
          END IF 
       
       END FUNCTION elapsedTime
+
       
       END MODULE timer_class
