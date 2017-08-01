20794608697c5a4c164c58db7c517afcf2aaf1f1
Daniel Harenberg (Laptop) <dharenberg@ethz.ch>
2017-08-01 18:35:14 +0200

    minor bug

--- epss4/src_utilities/valuefunction_mod.f90
+++ epss4/src_utilities/valuefunction_mod.f90
@@ -21,7 +21,7 @@ pure function value(p, coeffs, grids)
 ! Get the policy functions for the entire state space, i.e. both individual and aggregate states
 ! This is the master subroutine, calling all module procedures below (which are appear in calling order)
 ! It it pure but for the OMP directives
-    use params_mod      ,only: jr,surv, pi_z, pi_eta, cmin, g, beta, theta, gamm, apmax
+    use params_mod      ,only: jr,surv, pi_z, pi_eta, trans_prob, cmin, g, beta, theta, gamm, apmax
     use makegrid_mod
     real(dp)            ,allocatable :: value(:,:,:,:,:,:)  ! could make optional
     type(tPolicies)                  ,intent(in) :: p      ! policies
@@ -30,19 +30,20 @@ pure function value(p, coeffs, grids)
 
     real(dp) ,dimension(:,:,:,:,:,:) ,allocatable :: cons
     real(dp) ,dimension(:,:,:) ,allocatable             :: xgridp, consp, vp   ! xgrid, consumption, and value function tomorrow
-    real(dp) ,dimension(:,:) ,allocatable                :: yp      ! income tomorrow, for every idiosyncr & aggr state
+    real(dp) ,dimension(:,:,:) ,allocatable                :: yp      ! income tomorrow, for every idiosyncr & aggr state
     real(dp) ,dimension(:), allocatable                       :: rp, mup ! risky return AND equity premium for every aggr state tomorrow
     real(dp)   :: kp, rfp, app_min                      ! tomorrow's capital, equity premium,risk-free rate, min aprime
     real(dp)   :: betatildej, evp_xc                            ! modified discount factor, expected value tomorrow
-    integer    :: nx,n_eta,nz, nj, nk, nmu, jc, muc, kc, zc, xc, ec
+    integer    :: nx,n_eta,n_trans,nz, nj, nk, nmu, jc, muc, kc, zc, xc, ec
 
     nx = size(p%apgrid,1)
     n_eta = size(p%apgrid,2)
+    n_trans=size(trans_prob)
     nz = size(p%apgrid,3)
     nj = size(p%apgrid,4)
     nk = size(p%apgrid,5)
     nmu= size(p%apgrid,6)
-    allocate(yp(n_eta,nz), rp(nz), mup(nz))
+    allocate(yp(n_trans, n_eta,nz), rp(nz), mup(nz))
     allocate(xgridp(nx,n_eta,nz), consp(nx,n_eta,nz), vp(nx,n_eta,nz))
     allocate(cons(nx,n_eta,nz,nj,nk,nmu), value(nx,n_eta,nz,nj,nk,nmu))
 
@@ -72,15 +73,15 @@ end function value
 
 pure subroutine calc_vars_tomorrow(coeffs,grids,jc,zc,kc,muc,kp,mup,rp,rfp,yp)
 ! Forecast kp, mup, and get corresonding prices  (might want to move into laws_of_motion)
-    use params_mod     ,only: nz, pi_z, jr, ej, etagrid
+    use params_mod     ,only: nz, pi_z, jr, ej, etagrid, trans_grid
     use laws_of_motion ,only: Forecast_k, Forecast_mu
     use income
 
     type(tCoeffs)   ,intent(in)  :: coeffs ! coefficients for laws of motion
     type(tAggGrids) ,intent(in)  :: grids  ! grids for aggregate states k and mu
     integer         ,intent(in)  :: jc, zc, kc, muc
-    real(dp)        ,intent(out) :: kp, mup(:), rp(:), rfp, yp(:,:)
-    integer :: zpc, nk, nmu
+    real(dp)        ,intent(out) :: kp, mup(:), rp(:), rfp, yp(:,:,:)
+    integer :: zpc, epc, nk, nmu
     real(dp), parameter :: crit = 1.0e-10
 
     nk = size(grids%k)
@@ -104,9 +105,11 @@ pure subroutine calc_vars_tomorrow(coeffs,grids,jc,zc,kc,muc,kp,mup,rp,rfp,yp)
     do zpc= 1,nz
         rp(zpc) = f_stock_return(kp, zeta(zpc), delta(zpc), rfp)
         if (jc+1>=jr) then
-            yp(:,zpc) = f_pensions(kp, zeta(zpc))
+            yp(:,:,zpc) = f_pensions(kp, zeta(zpc))
         else
-            yp(:,zpc) = ej(jc+1) * f_netwage(kp, zeta(zpc)) * etagrid(:,zpc)
+            do epc=1,size(etagrid,1)
+                yp(:,epc,zpc) = ej(jc+1) * f_netwage(kp, zeta(zpc)) * etagrid(epc,zpc) * trans_grid
+            enddo
         endif
     enddo
     if (rfp < rp(1)*(1.0 + sign(0.0001_dp,rp(1))) .and. grids%mu(muc)>0.0 ) then
@@ -200,44 +203,45 @@ end subroutine interp_policies_tomorrow
 
 pure function evp(ap, kappa, xgridp, consp, vp, rfp,rp, yp, zc, xc, ec, betatildej)
     use fun_lininterp
-    use params_mod, only : collateral_constraint, pi_z, pi_eta, nz, n_eta, g, cmin, theta, gamm
+    use params_mod, only : collateral_constraint, pi_z, pi_eta, trans_prob, nz, n_eta, n_trans, g, cmin, theta, gamm
 
     real(dp)                    :: evp  ! cons(xc,ec,zc,jc), evp
-    real(dp)                   ,intent(in)  :: ap, kappa, rfp, betatildej, rp(:),yp(:,:), xgridp(:,:,:),consp(:,:,:), vp(:,:,:)
+    real(dp)                   ,intent(in)  :: ap, kappa, rfp, betatildej, rp(:),yp(:,:,:), xgridp(:,:,:),consp(:,:,:), vp(:,:,:)
     integer                    ,intent(in)  :: zc, xc, ec
     real(dp)   ,dimension(n_eta)            :: cons_interp, vp_interp ! interpolated values of cons, vp
     real(dp)   ,dimension(nz)               :: evpz, rhs_temp ! temporary: cee_*: consumption euler eq.
     real(dp)                                :: rtildep, xp    ! rtilde and cash-at-hand tomorrow
     real(dp)                                :: cee_rhs, rhs_fac1, rhs_fac2 ! temporary: rhs of cee, factors 1 and 2
-    integer                                 :: zpc, epc       ! counters for shocks tomorrow
+    integer                                 :: zpc, epc, tpc       ! counters for shocks tomorrow
+
+    evpz     = 0.0
+    rhs_temp = 0.0
 
     do zpc=1,nz
-        if (pi_z(zc,zpc) == 0.0) then
-            evpz(zpc)     = 0.0
-            rhs_temp(zpc) = 0.0
-            cycle
-        endif
+        if (pi_z(zc,zpc) == 0.0) cycle
 
         ! Get cash-at-hand tomorrow for all states
         rtildep= (1.0+rfp+kappa*(rp(zpc)-rfp))/(1.0+g)
-        do epc = 1,n_eta
-            xp = yp(epc,zpc)+rtildep*ap
+        do tpc = 1,n_trans
+            do epc = 1,n_eta
+                xp = yp(tpc,epc,zpc)+rtildep*ap
 
-            ! Interpolate consumption and value function
-            cons_interp(epc) = f_lininterp(xgridp(:,epc,zpc),consp(:,epc,zpc),xp)
-            vp_interp(epc)   = f_lininterp(xgridp(:,epc,zpc),vp(:,epc,zpc),xp)
-            if (cons_interp(epc) <=cmin) then
-                cons_interp(epc) = cmin
-                xp               = cmin + ap
+                ! Interpolate consumption and value function
+                cons_interp(epc) = f_lininterp(xgridp(:,epc,zpc),consp(:,epc,zpc),xp)
                 vp_interp(epc)   = f_lininterp(xgridp(:,epc,zpc),vp(:,epc,zpc),xp)
-            endif
-            if (vp_interp(epc) <=cmin) then
-                vp_interp(epc)   = cmin
-            endif
+                if (cons_interp(epc) <=cmin) then
+                    cons_interp(epc) = cmin
+                    xp               = cmin + ap
+                    vp_interp(epc)   = f_lininterp(xgridp(:,epc,zpc),vp(:,epc,zpc),xp)
+                endif
+                if (vp_interp(epc) <=cmin) then
+                    vp_interp(epc)   = cmin
+                endif
 
+            enddo
+            ! Now different factors of consumption euler equation
+            evpz(zpc)     = evpz(zpc) + trans_prob(tpc)* sum(pi_eta(ec,:)*vp_interp**(1.0-theta))
         enddo
-        ! Now different factors of consumption euler equation
-        evpz(zpc)     = sum(pi_eta(ec,:)*vp_interp**(1.0-theta))
     enddo
 
     evp=dot_product(pi_z(zc,:),evpz)    ! Expected V'
