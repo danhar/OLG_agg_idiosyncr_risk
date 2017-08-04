bdac62c4608f61e69a229feb7d3e180902b19707
Daniel Harenberg (Laptop) <dharenberg@ethz.ch>
2017-08-04 14:07:20 +0200

    bug

--- epss4/params_mod.f90
+++ epss4/params_mod.f90
@@ -1035,11 +1035,6 @@ use omp_lib           ,only: OMP_get_max_threads
         call critical_stop
     endif
 
-    if ((tau .ne. 0.00_dp) .and. (tau .ne. 0.02_dp) .and. (tau .ne. 0.04_dp) .and. (tau .ne. 0.12_dp) .and. (tau .ne. 0.14_dp) .and. (tau .ne. 0.16_dp)) then
-        print*, 'ERROR: the larger (after-reform) tau must be 0.02 or 0.14 (see to_do.txt)'
-!        call critical_stop
-    endif
-
     if (cover_k > 1.0 .or. cover_k < 0.0) then
         print*, 'ERROR: cover_k not in [0.0 , 1.0]'
         call critical_stop
