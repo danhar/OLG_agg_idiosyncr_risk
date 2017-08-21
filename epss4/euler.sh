11b1e46b82f3a25708c37480d36d19c640a1a315
Daniel Harenberg (Laptop) <dharenberg@ethz.ch>
2017-08-21 06:24:09 +0200

    jobs

--- epss4/euler.sh
+++ epss4/euler.sh
@@ -4,7 +4,7 @@
 # Copyright (c) 2010-2017 Daniel Harenberg - All rights reserved.
 #*******************************************************************************
 
-thisrun='100-base_tau2_newproc2'
+thisrun='102-ep2ext_norpfmed2'
 BUILD=Build_Parallel_Optim #Optimmax #Debugmax #Parallel_Debug # Parallel_Optim
 NTHREADS=24  # number of OpenMP threads
 # OMP_STACKSIZE=16M #stack size for each OMP thread: default 4M, recommended 16M, mytest 512M
